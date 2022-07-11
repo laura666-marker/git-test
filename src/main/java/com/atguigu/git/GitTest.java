f4c033740ee1bd96f6cd84dd70dfe650a532cfa9
yeshu <2283606091@qq.com>
2022-07-11 17:14:00 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author ysstart
+ * @create 2022-07-11 17:09
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
